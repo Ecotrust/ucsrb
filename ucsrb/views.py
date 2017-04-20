796ed7d8bc950d9ffdc863b7fbe1b30d8da8da7a
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-04-20 08:22:34 -0700

    adding some ugly templates as placeholders

--- ucsrb/views.py
+++ ucsrb/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("UCSRB is up.")
+    template = loader.get_template('index.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
