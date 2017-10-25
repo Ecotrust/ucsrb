c9db9d45f737842fd1018045f1612415fe03c85f
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-10-25 13:23:31 -0700

    adding get_segment_by_id to API

--- ucsrb/urls.py
+++ ucsrb/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     ### API urls
     url(r'^get_segment_by_bbox', views.get_segment_by_bbox),
+    url(r'^get_segment_by_id', views.get_segment_by_id),
 
 
     ### end API urls
