67ffc270aaf9cf0c99e5bd24fdf52d0da7096ea6
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-10-25 19:25:00 -0700

    most dummy API views done, most geo data in EPSG:3857

--- ucsrb/urls.py
+++ ucsrb/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     ### API urls
     url(r'^get_segment_by_bbox', views.get_segment_by_bbox),
     url(r'^get_segment_by_id', views.get_segment_by_id),
+    url(r'^get_pourpoint_by_id', views.get_pourpoint_by_id),
+    url(r'^filter_results', views.filter_results),
+    url(r'^get_results_by_scenario_id', views.get_results_by_scenario_id),
+    url(r'^get_results_by_state', views.get_results_by_state),
 
 
     ### end API urls
