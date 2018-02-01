9fba1c2a5766c504bc91920bec0a3e977284fd32
David Pollard <pollardld@gmail.com>
2018-01-31 16:55:42 -0800

    Nav transition on scenario selection
    add transform and animation to app nav

--- ucsrb/static/ucsrb/js/app.events.js
+++ ucsrb/static/ucsrb/js/app.events.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
   $('#scenario-nav button').click(function(event) {
     app.setState(event.currentTarget.dataset.scenarioType)
-    $('#scenario-nav').addClass('icons-only');
-    $('#scenario-nav').removeClass('col-9');
-    $('#scenario-nav').addClass('col-4');
-    $(`<div class="col-5"></div>`).insertAfter('#scenario-nav');
-    $('#file-nav').addClass('less-padding');
+    $('.nav-wrap').addClass('icons-only');
   });
   /**
    * for panel
