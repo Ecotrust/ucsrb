d1a822dd42cc4d94e221143adb6f0f4978b3f30d
Ryan Hodges <ryan.d.hodges@gmail.com>
2021-01-27 17:39:50 -0800

    upload functional testing complete

--- ucsrb/tests/test_functional.py
+++ ucsrb/tests/test_functional.py
@@ -1,5 +1,10 @@
 import os, time
 from selenium import webdriver
+
+# https://selenium-python.readthedocs.io/waits.html
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 # import unittest
 
 from django.test import TestCase, Client
@@ -25,6 +30,7 @@ from ucsrb.models import TreatmentScenario
 class RegisteredUserTest(TestCase):
     def setUp(self):
         try:
+            webdriver.DesiredCapabilities.FIREFOX["unexpectedAlertBehaviour"] = "accept"
             self.browser = webdriver.Firefox()
         except Exception as e:
             print(e)
@@ -37,7 +43,7 @@ class RegisteredUserTest(TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    # Reminder: This listerally has to start with the word "test_"
+    # Reminder: These literally have to start with the characters "test_"
     def test_anonymous_user_can_select_basin_to_define_treatment(self):
         print("Running Anonymous User Select Basin")
         print("TODO...")
@@ -67,9 +73,9 @@ class RegisteredUserTest(TestCase):
 
         # Alice has a pre-existing shapefile of a plot she'd like to treat
         # She saved it to her hard drive
-        self.assertTrue(os.path.exists('/usr/local/apps/marineplanner-core/apps/ucsrb/ucsrb/tests/test_files/'))
-        file_name = 'small_polygon_treatment_4326'
-        file_location = '/usr/local/apps/marineplanner-core/apps/ucsrb/ucsrb/tests/test_files/%s.zip' % file_name
+        self.assertTrue(os.path.exists('/usr/local/apps/marineplanner-core/apps/ucsrb/ucsrb/tests/test_files/multi-polygon/layers/'))
+        file_name = 'multi-treatment'
+        file_location = '/usr/local/apps/marineplanner-core/apps/ucsrb/ucsrb/tests/test_files/multi-polygon/layers/%s.zip' % file_name
         self.assertTrue(os.path.isfile(file_location))
 
         # She logs in to her SnowToFlow account...
@@ -96,25 +102,57 @@ class RegisteredUserTest(TestCase):
         self.browser.find_element_by_id("launch").click()
 
         # Opens the upload form...
-        self.assertTrue(len(self.browser.find_elements_by_id("upload-treatment-button")) == 1)
-        self.browser.find_element_by_id("upload-treatment-button").click()
+        self.assertTrue(len(self.browser.find_elements_by_class_name("btn-method-upload")) == 1)
+        self.browser.find_element_by_class_name("btn-method-define").click()
+        self.browser.find_element_by_class_name("btn-method-upload").click()
 
-        # Selects her file and submits.
+
+        # Selects her file, names her treatment, and submits.
         # http://allselenium.info/file-upload-using-python-selenium-webdriver/#:~:text=Python%20Selenium%20Webdriver%20comes%20with,the%20file%20to%20be%20uploaded.
         self.browser.find_element_by_id("id_zipped_shapefile").send_keys(file_location)
-        # TODO: Test projection?
-        self.browser.find_element_by_id("upload-treatment-form").find_element_by_id("submit").click()
 
-        # Her file is uploaded, unzipped and interpreted locally
-        # These tests should probably live in test_views - Alice don't care.
-        self.assertTrue(os.path.exists(os.path.join(settings.MEDIA_ROOT, "%s.shp" % file_name)))
-        time.sleep(1)
-        # Ater 1 second have we cleaned up?
-        self.assertFalse(os.path.exists(os.path.join(settings.MEDIA_ROOT, "%s.shp" % file_name)))
+        treatment_name = "Alice Test Treatment"
+        self.browser.find_element_by_id("id_treatment_name").send_keys(treatment_name)
+
+        # TODO: Test projection?
+
+        self.browser.find_element_by_id("upload-treatment-form").find_element_by_id("upload-submit-btn").click()
+
+        # She waits for the results to load
+        print("Waiting up to 10 seconds for scenario to load...")
+        try:
+            element = WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located((By.ID, "aggregate-results"))
+            )
+        except Exception as e:
+            self.browser.quit()
 
         # She is then presented with a report regarding different results based on how she wishes to treat her treatment area.
         self.assertTrue(self.browser.find_element_by_id("results-nav").is_displayed())
-        # TODO: test both Forest and Hydro data results.
 
-        # In the meantime, her scenario is saved, but her uploaded files are removed
-        # TODO: Test this in test_views.py
+        lt_20_pct_acres = self.browser.find_element_by_class_name("overflow-gradient").find_element_by_css_selector("table tr:nth-child(2) > td:nth-child(2)")
+        self.assertEqual(lt_20_pct_acres.text, "333")
+
+        # Why does Alice's define modal not disappear?
+        if self.browser.find_element_by_id("define-modal").is_displayed():
+            print("TODO: Figure out why Alice's 'Define' modal doesn't close itself")
+            self.browser.find_element_by_id("define-modal").find_element_by_class_name("close").click()
+            try:
+                element = WebDriverWait(self.browser, 10).until(
+                    EC.invisibility_of_element_located((By.CLASS_NAME, "modal-backdrop"))
+                )
+            except Exception as e:
+                self.browser.quit()
+
+        self.browser.find_element_by_css_selector("#results-nav button:nth-child(1)").click()
+        self.assertTrue(self.browser.find_element_by_id("hydro-note").is_displayed())
+        # How the heck are we going to get Alice to click on a pour point!?
+
+        self.browser.find_element_by_id("nav-load-save").click()
+
+        # In a moment of whimsy Alice deletes ALL of her saved treatments
+        saved_treatments = self.browser.find_element_by_id("load-saved-list").find_elements_by_css_selector("li")
+        for treatment in saved_treatments:
+            print("Deleting %s..." % treatment.text)
+            treatment.find_element_by_class_name("btn-trash").click()
+            treatment.find_element_by_class_name("confirm-delete").click()
--- ucsrb/tests/test_views.py
+++ ucsrb/tests/test_views.py
@@ -29,12 +29,12 @@ class upload_treatment_shapefileTest(TestCase):
     def test_upload_treatment_shapefile_kickoff(self):
         print("Testing Treatment Shapefile Upload")
 
-    def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('/ucsrb/upload_treatment_shapefile/')
-        self.assertEqual(response.status_code, 200)
+    # def test_view_url_exists_at_desired_location(self):
+    #     response = self.client.get('/ucsrb/upload_treatment_shapefile/')
+    #     self.assertEqual(response.status_code, 200)
 
-    def test_view_url_accessible_by_name(self):
-        request_url = '/ucsrb%s' % reverse('shp_upload')
-        print('Requesting: %s ...' % request_url)
-        response = self.client.get(request_url)
-        self.assertEqual(response.status_code, 200)
+    # def test_view_url_accessible_by_name(self):
+    #     request_url = '/ucsrb%s' % reverse('shp_upload')
+    #     print('Requesting: %s ...' % request_url)
+    #     response = self.client.get(request_url)
+    #     self.assertEqual(response.status_code, 200)
